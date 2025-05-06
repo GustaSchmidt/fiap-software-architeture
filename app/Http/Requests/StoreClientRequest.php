835c9c2bafc2d1cb9f9e108d6afdf4e26b949335
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2025-05-06 20:31:42 +0000

    CPF Format validator

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -17,7 +17,7 @@ class StoreClientRequest extends FormRequest
             'nome' => 'required|string',
             'sobrenome' => 'required|string',
             'email' => 'required|email|unique:clients,email',
-            'cpf' => 'required|string|unique:clients,cpf',
+            'cpf' => 'required|string|cpf|unique:clients,cpf',
             'senha' => 'required|string|size:32', // md5 hash
         ];
     }
