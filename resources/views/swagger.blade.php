d8e6144f70c6acf641ac5b581663f3a43330e97c
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2025-05-06 16:47:14 +0000

    swagger ui

--- resources/views/swagger.blade.php
+++ resources/views/swagger.blade.php
@@ -6,8 +6,21 @@
     <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
     <style>
       html { box-sizing: border-box; overflow: -moz-scrollbars-vertical; overflow-y: scroll; }
+      
       *, *:before, *:after { box-sizing: inherit; }
+      
       body { margin: 0; background: #fafafa; }
+      
+      .swagger-ui .opblock .opblock-summary-description {
+        color: #3b4151;
+        font-family: sans-serif;
+        font-size: 13px;
+        word-break: break-word;
+        margin-left: 20px;
+      }
+      .opblock-summary-method{
+        margin-right: 10px;
+      }
     </style>
   </head>
   <body>
