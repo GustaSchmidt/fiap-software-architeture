7de8c31a5cd4b83b3ef8eec80b215cc4ab5448ca
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2025-05-07 16:57:13 +0000

    swagger correction

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 
 # Docs
 Route::get('/', function () {
     return view('swagger');
+});
+
+Route::middleware([])->get('/swagger', function () {
+    $path = public_path('swagger.json');
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return Response::file($path, [
+        'Content-Type' => 'application/json',
+        'Content-Disposition' => 'inline; filename="swagger.json"',
+    ]);
 });
 
 # Produtos
