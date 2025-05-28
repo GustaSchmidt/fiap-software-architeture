97f2f039d58ac18f6c066f1f7e026f375dad8730
Gustavo Schmidt <gustavo.schmidt@ebanx.com>
2025-05-27 21:27:39 -0300

    return to defaults

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,21 +6,25 @@ git config --global --add safe.directory /var/www
 
 # Espera o banco de dados estar pronto (ajuste conforme o nome do serviço no docker-compose)
 echo "Aguardando o banco de dados..."
-# until nc -z -v -w30 db 3306; do
-#   echo "Aguardando conexão com o banco de dados..."
-#   sleep 5
-# done
+until nc -z -v -w30 db 3306; do
+  echo "Aguardando conexão com o banco de dados..."
+  sleep 5
+done
 
 # Instala as dependências do PHP (se a pasta vendor não existir)
-# if [ ! -d "vendor" ]; then
+if [ ! -d "vendor" ]; then
   echo "Instalando dependências com Composer..."
   composer install
-# fi
-
-if [ "$DB_ENV" = "dev" ];
-  echo "Gerando APP_KEY..."
-  php artisan key:generate
 fi
+
+# Gera a chave da aplicação, se ainda não existir
+if [ ! -f ".env" ]; then
+  echo "Arquivo .env não encontrado. Abortando."
+  exit 1
+fi
+
+echo "Gerando APP_KEY..."
+php artisan key:generate
 
 # Executa as migrations (remova --seed se não quiser popular)
 echo "Executando migrations e seeders..."
