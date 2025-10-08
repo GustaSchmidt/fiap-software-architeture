8998a36ecefdec7be8d2c954719618843c2b2e63
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2025-10-08 02:46:05 +0000

    corrige caminho do diretório de origem para empacotamento da Lambda

--- terraform/auth/main.tf
+++ terraform/auth/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 # 1. Empacota o código da Lambda em um arquivo .zip
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "../../lambda_auth_cpf/" # Caminho para a pasta da Lambda
+  source_dir  = "../lambda_auth_cpf/" # Caminho para a pasta da Lambda
   output_path = "lambda_auth_cpf.zip"
 }
 
