314f209a820af4d70779e5a691b521857d0d76e5
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2025-11-28 11:44:34 -0300

    Tech challenge/faze3 (#32)
    * Adiciona função Lambda para autenticação de cliente por CPF e geração de token JWT, incluindo configuração do Terraform e ajustes no .gitignore.
    
    * Ajuste de permissao para printa os results do terraform
    
    * Data Base Terraform
    
    * centralizando as coisas
    
    * Ajustando o changed files
    
    * ajuste 3
    
    * testado um negocio aqui
    
    * aaa
    
    * remove teste
    
    * teste
    
    * aaa
    
    * teste de run do workflow
    
    * teste debug
    
    * ds
    
    * remove shell bash
    
    * corrige dependências entre jobs e adiciona nova linha em deploy_terraform_infra.yml
    
    * remove debug output step from Terraform plan job
    
    * adiciona continue-on-error nas etapas de Terraform para permitir postagem de erros no PR
    
    * corrige caminho do diretório de origem para empacotamento da Lambda
    
    * debug
    
    * corrige caminho do diretório de origem para empacotamento da Lambda
    
    * corrige caminho do diretório de origem e nome do arquivo para empacotamento da Lambda
    
    * atualiza caminho da origem do código da Lambda para usar a variável de caminho do projeto
    
    * adiciona variável de ambiente TF_VAR_project_root_path para o diretório do projeto na configuração do Terraform
    
    * adiciona empacotamento e upload do código da Lambda para o S3, atualiza a configuração do Terraform para usar a versão do código
    
    * adiciona configuração de credenciais da AWS para o fluxo de trabalho de implantação
    
    * atualiza o empacotamento e upload do código da Lambda para incluir o nome da branch no artefato
    
    * atualiza o nome do bucket S3 para artefatos de código da Lambda
    
    * remove AWS_ACCOUNT_ID do nome do bucket S3 para artefatos de código da Lambda
    
    * substitui o nome da branch por 'pull_request_test' na configuração do fluxo de trabalho
    
    * remove variáveis de nome da função Lambda e do API Gateway; adiciona variável para chave S3 do código da Lambda
    
    * only for trigger
    
    * teste
    
    * teste de nome do job
    
    * Ajuste nome do deploy
    
    * teste value default
    
    * try 2
    
    * teste do valor default
    
    * Fix upload actifact
    
    * data bucket change
    
    * AAAA VOU USAR O EXPORT PELA SACO
    
    * bla bla
    
    * Remove import step for existing S3 bucket from Terraform deployment workflow

--- terraform/auth/main.tf
+++ terraform/auth/main.tf
@@ -17,7 +17,6 @@ provider "aws" {
 
 # Bucket S3 para armazenar os pacotes de código da Lambda
 resource "aws_s3_bucket" "lambda_artifacts" {
-  # Constrói um nome de bucket único usando o ID da conta AWS
   bucket = "fiap-soat-lambda-code-artifacts"
 
   tags = {
