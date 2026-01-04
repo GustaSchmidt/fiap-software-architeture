13c1eaa047515e2b00d1fa2da7196d8826dd35a8
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2026-01-04 11:53:03 -0300

    Deploy k8 aws (#42)
    * Add Kubernets service
    
    * make github happy
    
    * Remove dependencies and conditions from K8s deployment job
    
    * FUCK NAMES
    
    * Remove variável aws_region do arquivo de variáveis do Kubernetes
    
    * Adicionar variáveis de saída para o banco de dados e reinstaurar a variável aws_region
    
    * Comentar condição de execução para o job de infraestrutura do banco de dados
    
    * Comentar condição de execução para o passo de aplicação do Terraform no job de infraestrutura do banco de dados
    
    * Comentar remoção da dependência do job de infraestrutura do banco de dados
    
    * add stage default
    
    * Ajustes node modules
    
    * dsa
    
    * Ajuste db conection

--- terraform/database/outputs.tf
+++ terraform/database/outputs.tf
@@ -3,10 +3,34 @@ output "db_endpoint" {
   value       = aws_db_instance.default.endpoint
 }
 
+output "db_host" {
+  description = "O endereço (host) do banco de dados RDS (apenas DNS, sem porta)"
+  value       = aws_db_instance.default.address
+}
+
 output "db_port" {
   description = "A porta do banco de dados RDS"
   value       = aws_db_instance.default.port
 }
+
+output "db_name" {
+  description = "Nome do banco de dados"
+  value       = aws_db_instance.default.db_name
+}
+
+output "db_username" {
+  description = "Usuário master do banco"
+  value       = aws_db_instance.default.username
+}
+
+output "db_password" {
+  description = "Senha master do banco (Sensível)"
+  value       = aws_db_instance.default.password
+  sensitive   = true
+}
+
+
+# --- Outros Outputs tenho que mudar essa bucha pra rede ser criada a parte (outro TF)---
 
 output "subnet_ids" {
   description = "List of IDs for the database subnets"
