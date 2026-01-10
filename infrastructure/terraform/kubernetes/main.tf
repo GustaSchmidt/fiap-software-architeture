07aaf44a3d3ac7947c1fcd9957b82a5c3fca1b29
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2026-01-10 22:20:15 +0000

    Ajustes do CD

--- infrastructure/terraform/kubernetes/main.tf
+++ infrastructure/terraform/kubernetes/main.tf
@@ -78,11 +78,24 @@ resource "aws_ecr_repository" "app_repo" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_repository" "payment_repo" {
+  name                 = "fiap-soat-payment"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true 
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
 output "ecr_repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
 
+output "payment_repository_url" {
+  description = "URL do repositório ECR do Payment Service"
+  value       = aws_ecr_repository.payment_repo.repository_url
+}
 
 # --- Monitoramento da aplicação com cloudwatch ---
 resource "aws_eks_addon" "cloudwatch_observability" {
